25ce188af0d57f1871907ce661ce3d797e1fdffc
Ching-Wei Lai <103228835+1640Wei@users.noreply.github.com>
2023-11-01 15:59:33 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
+from PIL import Image, ImageTk
 from snow_alert import snow_alert
 
 # 添加 snow_alert.py 所在目录到系统路径中
@@ -18,21 +19,55 @@ def on_set_alert():
     snow_alert(selected_city, selected_email, selected_time)
     messagebox.showinfo("Alert Set", f"Snow alert set for {selected_city} at {selected_time}.")
 
+def resize_image(image, new_width, new_height):
+    return image.resize((new_width, new_height), Image.LANCZOS)
+
 root = tk.Tk()
 root.title("Snow Alert App")
+root.geometry("350x450")
 
+# icon
+icon_path = "C:/Users/Ching Wei Lai/Desktop/pythonProject/winter.png"
+original_image = Image.open(icon_path)
+
+# 縮放圖片以填滿整個窗口背景
+window_width = 350
+window_height = 450
+resized_image = resize_image(original_image, window_width, window_height)
+
+# 將Image對象轉換為PhotoImage對象
+icon_image = ImageTk.PhotoImage(resized_image)
+
+# 創建一個Label元素，將其背景設置為圖片
+background_label = ttk.Label(root, image=icon_image)
+background_label.place(relx=0, rely=0, relwidth=1, relheight=1)  # 設置Label充滿整個窗口
+
+title_style = ('Times', 20, 'bold')
+font_style = ('Century', 13)
+
+label_title = tk.Label(root, text="Snow Alert", font=title_style, foreground="white")
 canadian_cities = ["Toronto", "Montreal", "Vancouver", "Calgary", "Ottawa", "Edmonton", "Winnipeg", "Quebec City"]
-label_city = tk.Label(root, text="Select City:")
-city_combobox = ttk.Combobox(root, values=canadian_cities)
+label_city = tk.Label(root, text="Select City:", font=font_style)
+city_combobox = ttk.Combobox(root, values=canadian_cities, font=font_style)
 city_combobox.set("Toronto")
 
-label_email = tk.Label(root, text="Enter Your Email:")
-entry_email = tk.Entry(root)
+label_email = tk.Label(root, text="Enter Your Email:", font=font_style)
+entry_email = tk.Entry(root, font=font_style)
 
-label_time = tk.Label(root, text="Set Alert Time (HH:MM):")
-entry_time = tk.Entry(root)
-button_set_alert = tk.Button(root, text="Set Alert", command=on_set_alert)
+label_time = tk.Label(root, text="Set Alert Time (HH:MM):", font=font_style)
+entry_time = tk.Entry(root, font=font_style)
+button_set_alert = tk.Button(root, text="Set Alert", command=on_set_alert, font=font_style)
 
+label_title.configure(background='#1a6985')
+label_city.configure(background='#bee0ec')
+city_combobox.configure(background='#d3eaf2')
+label_email.configure(background='#bee0ec')
+entry_email.configure(background='#e9f5f9')
+label_time.configure(background='#bee0ec')
+entry_time.configure(background='#e9f5f9')
+button_set_alert.configure(background='#e9f5f9')
+
+label_title.pack(pady=10)
 label_city.pack(pady=10)
 city_combobox.pack(pady=10)
 label_email.pack(pady=10)
@@ -40,5 +75,6 @@ entry_email.pack(pady=10)
 label_time.pack(pady=10)
 entry_time.pack(pady=10)
 button_set_alert.pack(pady=20)
+
 
 root.mainloop()
