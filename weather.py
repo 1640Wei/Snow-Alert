15e10c4643ce248b0b7055b2cd32ef5b64f89d22
Ching-Wei Lai <103228835+1640Wei@users.noreply.github.com>
2023-11-01 09:37:55 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -4,7 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def umbrella_reminder():
+def snow_alert():
     city = "Toronto"
 
     # creating url and requests instance
@@ -13,39 +13,32 @@ def umbrella_reminder():
 
     # getting raw data
     soup = BeautifulSoup(html, 'html.parser')
-    temperature = soup.find('div',
-                            attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
-    time_sky = soup.find('div',
-                         attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
+    temperature = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
+    time_sky = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
 
     # formatting data
     sky = time_sky.split('\n')[1]
 
-    if sky == "Rainy" or sky == "Rain And Snow" or sky == "Showers" or sky == "Haze" or sky == "Cloudy":
+    if "Snow" in sky:
         smtp_object = smtplib.SMTP('smtp.gmail.com', 587)
-
-        # start TLS for security
         smtp_object.starttls()
 
         # Authentication
         smtp_object.login("YOUR EMAIL", "PASSWORD")
-        subject = "GeeksforGeeks Umbrella Reminder"
-        body = f"Take an umbrella before leaving the house.\
-                Weather condition for today is {sky} and temperature is {temperature} in {city}."
-        msg = f"Subject:{subject}\n\n{body}\n\nRegards,\nGeeksforGeeks".encode('utf-8')
+
+        subject = "Snow Alert"
+        body = f"Be prepared for snow! Weather condition for today is {sky} and temperature is {temperature} in {city}."
+        msg = f"Subject:{subject}\n\n{body}\n\nRegards,\nYour Name".encode('utf-8')
 
         # sending the mail
-        smtp_object.sendmail("FROM EMAIL",
-                             "TO EMAIL", msg)
+        smtp_object.sendmail("FROM EMAIL", "TO EMAIL", msg)
 
         # terminating the session
         smtp_object.quit()
-        print("Email Sent!")
+        print("Snow Alert Email Sent!")
 
-    # Every day at 06:00AM time umbrellaReminder() is called.
-
-
-schedule.every().day.at("06:00").do(umbrella_reminder)
 
 while True:
     schedule.run_pending()
+
+
