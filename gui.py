38efe77ee38493d70491d1d06cb41fe75d39785a
Ching-Wei Lai <103228835+1640Wei@users.noreply.github.com>
2023-11-01 09:57:17 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,54 +1,49 @@
+# gui.py
+
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
-import schedule
-import smtplib
-import requests
-from bs4 import BeautifulSoup
-
-def snow_alert():
-    city = "Toronto"
-    url = "https://www.google.com/search?q=weather" + city
-    html = requests.get(url).content
-    soup = BeautifulSoup(html, 'html.parser')
-    temperature = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
-    time_sky = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
-    sky = time_sky.split('\n')[1]
-
-    if "Snow" in sky:
-        send_email()
-        messagebox.showinfo("Snow Alert", f"Be prepared for snow!\nWeather condition for today is {sky} and temperature is {temperature} in {city}.")
-
-def send_email():
-    smtp_object = smtplib.SMTP('smtp.gmail.com', 587)
-    smtp_object.starttls()
-    smtp_object.login("YOUR EMAIL", "PASSWORD")
-
-    subject = "Snow Alert"
-    body = f"Be prepared for snow! Weather condition for today is {sky} and temperature is {temperature} in {city}."
-    msg = f"Subject:{subject}\n\n{body}\n\nRegards,\nYour Name".encode('utf-8')
-
-    smtp_object.sendmail("FROM EMAIL", "TO EMAIL", msg)
-    smtp_object.quit()
-    print("Snow Alert Email Sent!")
-
-def set_schedule():
-    schedule.every().day.at(entry_time.get()).do(snow_alert)
+from snow_alert import snow_alert  # 导入 snow_alert 函数
 
 
+def on_set_alert():
+    selected_city = city_combobox.get()
+    selected_email = entry_email.get()
+    selected_time = entry_time.get()
+
+    # 调用 snow_alert 函数并传递参数
+    snow_alert(selected_city, selected_email, selected_time)
+    messagebox.showinfo("Alert Set", f"Snow alert set for {selected_city} at {selected_time}.")
+
+
+# 创建主窗口
 root = tk.Tk()
 root.title("Snow Alert App")
 
+# 预定义加拿大可能下雪的城市
+canadian_cities = ["Toronto", "Montreal", "Vancouver", "Calgary", "Ottawa", "Edmonton", "Winnipeg", "Quebec City"]
+label_city = tk.Label(root, text="Select City:")
+city_combobox = ttk.Combobox(root, values=canadian_cities)
+city_combobox.set("Toronto")
+
+label_email = tk.Label(root, text="Enter Your Email:")
+entry_email = tk.Entry(root)
 
 label_time = tk.Label(root, text="Set Alert Time (HH:MM):")
 entry_time = tk.Entry(root)
-button_set_alert = tk.Button(root, text="Set Alert", command=set_schedule)
+button_set_alert = tk.Button(root, text="Set Alert", command=on_set_alert)
 
-
+# 布局 GUI 元素
+label_city.pack(pady=10)
+city_combobox.pack(pady=10)
+label_email.pack(pady=10)
+entry_email.pack(pady=10)
 label_time.pack(pady=10)
 entry_time.pack(pady=10)
 button_set_alert.pack(pady=20)
 
-
+# 运行 Tkinter 主循环
 root.mainloop()
+
 
 
