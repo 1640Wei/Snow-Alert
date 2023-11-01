c96a532927ca3a411e0f01d376e83e2058ff2510
Ching-Wei Lai <103228835+1640Wei@users.noreply.github.com>
2023-11-01 13:06:34 -0500

    Update snow_alert.py

--- snow_alert.py
+++ snow_alert.py
@@ -2,7 +2,7 @@ import os
 import smtplib
 import requests
 from bs4 import BeautifulSoup
-def snow_alert(city, to_email):
+def snow_alert(city, to_email, time):
     # 创建 URL 和请求实例
     url = f"https: //www.google.com/search?q=weather{city}"
     html = requests.get(url).content
@@ -16,10 +16,10 @@ def snow_alert(city, to_email):
     sky = time_sky.split('\n')[1]
 
     if "Snow" in sky:
-        send_email(to_email, city, sky, temperature)
-        print(f"Snow Alert Email Sent for {city}!")
+        send_email(to_email, city, sky, temperature, time)
+        print(f"Snow Alert Email Sent for {city} at {time}!")
 
-def send_email(to_email, city, sky, temperature):
+def send_email(to_email, city, sky, temperature, time):
     # 发送电子邮件的逻辑
     smtp_object = smtplib.SMTP('smtp.gmail.com', 587)
     smtp_object.starttls()
@@ -33,11 +33,10 @@ def send_email(to_email, city, sky, temperature):
 
     subject = "Snow Alert"
     body = f"Be prepared for snow! Weather condition for today is {sky} and temperature is {temperature} in {city}."
-    msg = f"Subject: {subject}\n\n{body}\n\nRegards, \nYour Name".encode('utf-8')
+    msg = f"Subject: {subject}\n\n{body}\n\nRegards, \nYour Name\n Alert Time: {time}".encode('utf-8')
 
     # sending the mail
     smtp_object.sendmail(email, to_email, msg)
 
     # terminating the session
     smtp_object.quit()
-
