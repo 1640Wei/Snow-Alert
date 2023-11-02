8327972ca5898a0d6785dd25715820e643f7327a
Ching-Wei Lai <103228835+1640Wei@users.noreply.github.com>
2023-11-02 09:49:57 -0500

    Update snow_alert.py

--- snow_alert.py
+++ snow_alert.py
@@ -8,27 +8,22 @@ def snow_alert(city, to_email, time):
    url = f"https: //www.google.com/search?q=weather{city}"
    html = requests.get(url).content
 
-
    # Use the BeautifulSoup module to parse HTML content.
    soup = BeautifulSoup(html, 'html.parser')
    temperature = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
    time_sky = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
 
-
    # Check if the sky condition (sky) contains the string 'Snow'.
    sky = time_sky.split('\n')[1]
-
 
    if "Snow" in sky:
        send_email(to_email, city, sky, temperature, time)
        print(f"Snow Alert Email Sent for {city} at {time}!")
 
-
 def send_email(to_email, city, sky, temperature, time):
    # Establish a connection to the Gmail SMTP server.
    smtp_object = smtplib.SMTP('smtp.gmail.com', 587)
    smtp_object.starttls()
-
 
    # Authentication
    email = os.environ.get('YOUR_EMAIL')
@@ -40,11 +35,9 @@ def send_email(to_email, city, sky, temperature, time):
    body = f"Be prepared for snow! Weather condition for today is {sky} and temperature is {temperature} in {city}."
    msg = f"Subject: {subject}\n\n{body}\n\nRegards, \nYour Name\n Alert Time: {time}".encode('utf-8')
 
-
-   # sending the mail
+   # Sending the mail
    smtp_object.sendmail(email, to_email, msg)
 
-
-   # terminating the session
+   # Terminating the session
    smtp_object.quit()
     
