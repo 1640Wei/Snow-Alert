6199830dfd371e5677223961177dc7b43be2ee87
Ching-Wei Lai <103228835+1640Wei@users.noreply.github.com>
2023-11-02 12:05:03 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -5,49 +5,60 @@ from tkinter import ttk
 from tkinter import messagebox
 from PIL import Image, ImageTk
 from snow_alert import snow_alert
-
-# 添加 snow_alert.py 所在目录到系统路径中
 import sys
 import os
 
+
+# Include the directory with snow_alert.py in Python's module search path.
 snow_alert_path = os.path.join(os.path.dirname(__file__), 'snow_alert')
 sys.path.append(snow_alert_path)
 
+
+# Set snow alert in the GUI.
 def on_set_alert():
-    selected_city = city_combobox.get()
-    selected_email = entry_email.get()
-    selected_time = entry_time.get()
+   selected_city = city_combobox.get()
+   selected_email = entry_email.get()
+   selected_time = entry_time.get()
 
-    snow_alert(selected_city, selected_email, selected_time)
-    messagebox.showinfo("Alert Set", f"Snow alert set for {selected_city} at {selected_time}.")
 
+   snow_alert(selected_city, selected_email, selected_time)
+   messagebox.showinfo("Alert Set", f"Snow alert set for {selected_city} at {selected_time}.")
+
+
+# Resize the image.
 def resize_image(image, new_width, new_height):
-    return image.resize((new_width, new_height), Image.LANCZOS)
+   return image.resize((new_width, new_height), Image.LANCZOS)
 
 root = tk.Tk()
 root.title("Snow Alert App")
 root.geometry("350x430")
 
-#background image
+# background image
 image_url = "https://i.postimg.cc/GmkDKRK2/winter.png"
 image_data = urllib.request.urlopen(image_url).read()
 original_image = Image.open(io.BytesIO(image_data))
 
-# 縮放圖片以填滿整個窗口背景
+# Resize the image to fill the entire window background.
 window_width = 350
 window_height = 430
 resized_image = resize_image(original_image, window_width, window_height)
 
-# 將Image對象轉換為PhotoImage對象
+
+# Convert an Image object to a PhotoImage object.
 icon_image = ImageTk.PhotoImage(resized_image)
 
-# 創建一個Label元素，將其背景設置為圖片
-background_label = ttk.Label(root, image=icon_image)
-background_label.place(relx=0, rely=0, relwidth=1, relheight=1)  # 設置Label充滿整個窗口
 
+# Create a Label element and set its background to an image.
+background_label = ttk.Label(root, image=icon_image)
+background_label.place(relx=0, rely=0, relwidth=1, relheight=1)  
+
+
+# font
 title_style = ('Times', 20, 'bold')
 font_style = ('Century', 13)
 
+
+# Create labels for the remaining information.
 label_title = tk.Label(root, text="Snow Alert", font=title_style, foreground="white")
 canadian_cities = ["Toronto", "Montreal", "Vancouver", "Calgary", "Ottawa", "Edmonton", "Winnipeg", "Quebec City"]
 label_city = tk.Label(root, text="Select City:", font=font_style)
@@ -78,6 +89,5 @@ entry_email.pack(pady=10)
 label_time.pack(pady=10)
 entry_time.pack(pady=10)
 button_set_alert.pack(pady=15)
-
 
 root.mainloop()
